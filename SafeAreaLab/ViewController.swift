32dbe3f2d4bb24e1453df4e38839270c765d2caf
ogullcankara <ogullcankara@gmail.com>
2022-10-04 12:20:26 +0300

    Initial Commit

---
+++ SafeAreaLab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SafeAreaLab
+//
+//  Created by Ogulcan Kara on 4.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SafeAreaLabUITests/SafeAreaLabUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SafeAreaLabUITestsLaunchTests.swift
+//  SafeAreaLabUITests
+//
+//  Created by Ogulcan Kara on 4.10.2022.
+//
+
+import XCTest
+
+class SafeAreaLabUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
